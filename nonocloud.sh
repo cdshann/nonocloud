a1444395b3dd2aab053483d9cd817d24f4c3dae7
cdshann <craigshannonx@gmail.com>
2015-11-06 11:04:23 -0800

    added ssh key login and the ability to specify a preexisting backing image

--- nonocloud.sh
+++ nonocloud.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 
-DIR=`pwd` 
-
-# Change working directories
-cd "$(dirname "$0")"
+DIR=`pwd -P` 
 
 # Read config
 source nonocloud.conf 
 
+# Check if a preexisting image was specified
+if [ -z "$BASE_IMG" ]; then
+
 # Download the ubuntu server image and hash
-wget -O $FNAME $DL_URL$IMG
-wget -O hashes $DL_URL$HASH
+echo "[nonocloud] downloading trusty image hashes"
+wget -q -O hashes $DL_URL$HASH
+
+# Check if backing file already exists
+if [ -n "$FNAME" ]; then
+	echo "[nonocloud] found existing cloud image, checking hash"
+else
+	echo "[nonocloud] downloading trusty64 cloud image"
+	wget -O $FNAME $DL_URL$IMG
+fi
 
 # Check the downloaded hashes
 hash_check=`grep "$IMG" hashes | cut -d " " -f1`
 checksum=`sha256sum $FNAME | cut -d " " -f1`
 
 if [ "$hash_check" != "$checksum" ]; then
-	echo "Error: image hash does not verify"
-	exit 1
+	echo "[nonocloud] error: image hash does not verify"
+	echo "[nonocloud] attempting to re-download image"
+
+	# We only attempt to re-download the image once
+	wget -O $FNAME $DL_URL$IMG
+	checksum=`sha256sum $FNAME | cut -d " " -f1`
+
+	if [ "$hash_check" != "$checksum" ]; then
+		echo "[nonocloud] hash mismatch after re-downloading image. exiting"
+		exit -1
+	fi
+fi
+
+BASE_IMG=$FNAME
+
 fi
 
 # Create the overlay image
-qemu-img create -f qcow2 -b $FNAME trusty64.img $DISK_SIZE
+echo "[nonocloud] creating overlay image"
+qemu-img create -f qcow2 -b $BASE_IMG $NAME.qcow2 $DISK_SIZE
 
-# Create the cloud-init data
+if [ -n "$PASSWORD" ]; then
+
+echo "[nonocloud] configuring password login"
+# Create the cloud-init data for pw auth
 cat > cloudinit.data <<EOF
 #cloud-config
 apt_update: true
@@ -36,16 +61,47 @@ manual_cache_clean: True
 
 packages:
  - fail2ban
-
 EOF
 
+elif [ -n "$SSH" ]; then
+echo "[nonocloud] configuring ssh login"
+# Create the cloud-init data for pw auth
+cat > cloudinit.data <<EOF
+#cloud-config
+ssh_authorized_keys:
+ - $SSH
+manual_cache_clean: True
+apt_update: true
+apt_upgrade: true
+packages:
+ - fail2ban
+EOF
+
+
+else
+echo "[nonocloud] no password or SSH key found, aborting"
+fi
+
 # Use cloud-localds to create the nocloud-data data source
+echo "[nonocloud] creating cloudinit data source"
 cloud-localds cloudinit.iso cloudinit.data
 
 # Create the VM using virt-install. Edit to reflect your environemnt
-virt-install --connect qemu:///system --name $NAME --ram $RAM --vcpus $CPU --disk path=$DIR/trusty64.img,format=qcow2,bus=virtio --cdrom cloudinit.iso --network=bridge:br0,model=virtio --os-type=linux --os-variant=ubuntutrusty --noautoconsole
+echo "[nonocloud] provisioning vm"
+virt-install --connect qemu:///system \
+--name $NAME \
+--ram $RAM \
+--vcpus $CPU \
+--disk path=$DIR/$NAME.qcow2,format=qcow2,bus=virtio \
+--cdrom cloudinit.iso \
+--network=bridge:$IFACE,model=virtio \
+--os-type=linux \
+--os-variant=ubuntutrusty \
+--noautoconsole
 
 # Clean up
 rm -f cloudinit.iso
 rm -f cloudinit.data
 rm -f hashes
+
+echo "[nonocloud] complete. please use virsh to interact with your new vm"
